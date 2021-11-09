0f266006e538e286f8c35c6ebeba307e0a428dfc
Александр Дайнеко <adaineko@65apps.com>
2021-11-09 13:42:24 +0700

    Add update with optimistic lock

--- atomics/src/main/java/name/shokred/Test.java
+++ atomics/src/main/java/name/shokred/Test.java
@@ -1,7 +1,23 @@
 package name.shokred;
 
-public class Test {
-    public static void main(String[] args) {
+import java.util.concurrent.atomic.AtomicBoolean;
 
+public class Test {
+    private ComplexObject state;
+    private String version;
+    private final AtomicBoolean locked = new AtomicBoolean(false);
+
+    public void updateState(ComplexObject newState) {
+        String newVersion;
+        do {
+            newVersion = getVersion();
+        } while (locked.compareAndSet(false, true));
+        this.state = newState;
+        this.version = newVersion;
+        this.locked.set(false);
+    }
+
+    public String getVersion() {
+        throw new UnsupportedOperationException();
     }
 }

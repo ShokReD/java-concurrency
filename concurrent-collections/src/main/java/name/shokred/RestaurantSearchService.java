6b48685023d5fb0eda537ff35f077f485da7ab9d
Александр Дайнеко <adaineko@65apps.com>
2021-11-21 00:26:13 +0700

    Add solution for task

--- concurrent-collections/src/main/java/name/shokred/RestaurantSearchService.java
+++ concurrent-collections/src/main/java/name/shokred/RestaurantSearchService.java
@@ -1,20 +1,26 @@
 package name.shokred;
 
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.stream.Collectors.toSet;
 
 public class RestaurantSearchService {
-//    private X stat;
+    private final Map<String, Integer> stat = new ConcurrentHashMap<>();
 
     public Restaurant getByName(final String restaurantName) {
         addToStat(restaurantName);
-        throw new UnsupportedOperationException();
+        return new Restaurant();
     }
 
     public void addToStat(final String restaurantName) {
-        throw new UnsupportedOperationException();
+        this.stat.compute(restaurantName, (name, count) -> count == null ? 1 : count + 1);
     }
 
     public Set<String> printStat() {
-        throw new UnsupportedOperationException();
+        return this.stat.entrySet().stream()
+                .map(entry -> String.format("%s - %s", entry.getKey(), entry.getValue()))
+                .collect(toSet());
     }
 }

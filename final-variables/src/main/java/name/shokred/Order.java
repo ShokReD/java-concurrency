881837b41ca4555286da4a08a13e73965f5e3d13
Александр Дайнеко <adaineko@65apps.com>
2021-11-27 15:52:12 +0700

    Make items final in order

--- final-variables/src/main/java/name/shokred/Order.java
+++ final-variables/src/main/java/name/shokred/Order.java
@@ -3,27 +3,31 @@ package name.shokred;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Collections;
 import java.util.List;
 
 @Getter
 @Setter
 public class Order {
     private Long id;
-    private List<Item> items;
+    private final List<Item> items;
     private PaymentInfo paymentInfo;
     private boolean isPacked;
     private Status status;
 
     public Order(final List<Item> items) {
-        this.items = items;
+        this.items = orEmpty(items);
     }
 
     public synchronized boolean checkStatus() {
-        if (items != null && !items.isEmpty() && paymentInfo != null && isPacked) {
+        if (!items.isEmpty() && paymentInfo != null && isPacked) {
             status = Status.DELIVERED;
             return true;
         }
         return false;
     }
 
+    private static <T> List<T> orEmpty(final List<T> list) {
+        return list == null ? Collections.emptyList() : list;
+    }
 }

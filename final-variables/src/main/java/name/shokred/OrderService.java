5096384924bbd900bbb37539a116469d1c843c1d
Александр Дайнеко <adaineko@65apps.com>
2021-12-01 23:28:49 +0700

    Change HashMap to ConcurrentHashMap for storing orders

--- final-variables/src/main/java/name/shokred/OrderService.java
+++ final-variables/src/main/java/name/shokred/OrderService.java
@@ -1,42 +1,47 @@
 package name.shokred;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class OrderService {
 
-    private final Map<Long, Order> currentOrders = new HashMap<>();
+    private final Map<Long, Order> currentOrders = new ConcurrentHashMap<>();
     private final AtomicLong nextId = new AtomicLong(0L);
 
     private long nextId() {
         return nextId.getAndIncrement();
     }
 
-    public synchronized long createOrder(final List<Item> items) {
+    public long createOrder(final List<Item> items) {
         final long id = nextId();
         final Order order = new Order(id, items);
         currentOrders.put(id, order);
         return id;
     }
 
-    public synchronized void updatePaymentInfo(final long cartId,
-                                               final PaymentInfo paymentInfo) {
-        currentOrders.get(cartId).setPaymentInfo(paymentInfo);
-        if (currentOrders.get(cartId).checkStatus()) {
-            deliver(currentOrders.get(cartId));
-            currentOrders.get(cartId).setStatus(Status.DELIVERED);
-        }
+    public void updatePaymentInfo(final long cartId, final PaymentInfo paymentInfo) {
+        currentOrders.computeIfPresent(cartId, (id, order) -> {
+            order.setPaymentInfo(paymentInfo);
+            if (order.checkStatus()) {
+                deliver(order);
+                order.setStatus(Status.DELIVERED);
+            }
+            return order;
+        });
     }
 
-    public synchronized void setPacked(final long cartId) {
-        currentOrders.get(cartId).setPacked(true);
-        if (currentOrders.get(cartId).checkStatus()) {
-            deliver(currentOrders.get(cartId));
-            currentOrders.get(cartId).setStatus(Status.DELIVERED);
-        }
+    public void setPacked(final long cartId) {
+        currentOrders.computeIfPresent(cartId, (id, order) -> {
+            order.setPacked(true);
+            if (order.checkStatus()) {
+                deliver(order);
+                order.setStatus(Status.DELIVERED);
+            }
+            return order;
+        });
     }
 
-    private synchronized void deliver(final Order order) { /*...*/ }
+    private void deliver(final Order order) { /*...*/ }
 }
